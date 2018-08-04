bd480031b666e3dc56c343f330660dc4e7e4fe6a
Carter Duncan <dukeblue2017@gmail.com>
2018-08-04 00:02:58 -0500

    Further progress

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App.jsx';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 import "typeface-roboto";
 import "typeface-bungee";
 import "typeface-comfortaa";
